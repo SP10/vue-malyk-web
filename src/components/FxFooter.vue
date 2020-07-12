5fb77a4a564be02cffc4770c8ee08721c5e6b19f
Serhii Pavlovych <programmersp@gmail.com>
2020-07-12 23:27:03 +0300

    Add instagram icon

--- src/components/FxFooter.vue
+++ src/components/FxFooter.vue
@@ -2,7 +2,7 @@
   <v-footer padless fixed color="#FFF8E1">
     <v-card class="flex" tile >
       <v-card-title class="pa-0">
-          <span class="ml-1 text-sm-subtitle-2 font-weight-light">Долучайтесь :)</span>
+          <span class="ml-1 text-sm-subtitle-2 font-weight-light">Приєднуйтесь до нас</span>
         <v-spacer></v-spacer>
         <v-btn v-for="icon in icons" :href="icon.path" :key="icon.key" class="mx-4" color="primary"  dark icon>
           <v-icon size="24px">{{ icon.icon }}</v-icon>
@@ -16,7 +16,10 @@
 export default {
   name: "fx-footer",
   data: () => ({
-    icons: [{key: 0, icon:"mdi-facebook", path:'https://www.facebook.com/MalykPartners/'}]
+    icons: [
+      {key: 0, icon:"mdi-facebook", path:'https://www.facebook.com/MalykPartners/'},
+      {key: 1, icon:"mdi-instagram", path:'https://instagram.com/malykpartners'}
+    ]
   })
 };
 </script>
