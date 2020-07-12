b4f0b27b95e75dfa440cc5011ccf2e842e8f46b0
Serhii Pavlovych <programmersp@gmail.com>
2020-07-12 20:13:40 +0300

    Remove import of component FxSystemBar from FxHeader

--- src/components/FxHeader.vue
+++ src/components/FxHeader.vue
@@ -1,28 +1,28 @@
 <template>
-  <v-card class="overflow-hidden" outlined>
+  <v-card class='overflow-hidden' outlined>
     <v-app-bar
       absolute
-      color="#fcb69f"
+      color='#fcb69f'
       dark
       flat
       shrink-on-scroll
-      src="../assets/hero.jpg"
-      scroll-target="#scrolling-techniques-2"
+      src='../assets/hero.jpg'
+      scroll-target='#scrolling-techniques-2'
     >
-      <template v-slot:img="{ props }">
+      <template v-slot:img='{ props }'>
         <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+          v-bind='props'
+          gradient='to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)'
         ></v-img>
       </template>
 
-      <v-app-bar-nav-icon @click="drawer = true" color="primary"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click='drawer = true' color='primary'></v-app-bar-nav-icon>
 
-      <v-toolbar-title class="text-caption text-sm-h6" color="primary">{{ title }}</v-toolbar-title>
+      <v-toolbar-title class='text-caption text-sm-h6' color='primary'>{{ title }}</v-toolbar-title>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" right="right" absolute temporary>
+    <v-navigation-drawer v-model='drawer' right='right' absolute temporary>
       <v-list nav dense>
-        <v-list-item v-for="item in items" :key="item.title" @click="page(items.path)">
+        <v-list-item v-for='item in items' :key='item.title' :to='items.path'>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -33,35 +33,25 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-sheet id="scrolling-techniques-2" class="overflow-y-auto" max-height="600">
-      <v-container style="height: 1000px;"></v-container>
+    <v-sheet id='scrolling-techniques-2' class='overflow-y-auto' max-height='600'>
+      <v-container style='height: 1000px;'></v-container>
     </v-sheet>
   </v-card>
 </template>
 
 <script>
-
-
 export default {
-  name: "fx-header",
-  components:{
-    FxSystemBar
-  },
+  name: 'fx-header',
   data: () => ({
-    title: "Адвокатське бюро Малик та партнери",
+    title: 'Адвокатське бюро Малик та партнери',
     drawer: false,
     items: [
-      { title: "Головна", icon: "mdi-home", path: '' },
-      { title: "Команда", icon: "mdi-account-group" },
-      { title: "Про нас", icon: "mdi-information" },
-      { title: "Консультація", icon: "mdi-account-question" }
-    ]
+      { title: 'Головна', icon: 'mdi-home', path: '' },
+      { title: 'Команда', icon: 'mdi-account-group' },
+      { title: 'Про нас', icon: 'mdi-information' },
+      { title: 'Консультація', icon: 'mdi-account-question' },
+    ],
   }),
-  methods: {
-     page(path){
-      console.log(path);
-    }
-  }
 };
 </script>
 
