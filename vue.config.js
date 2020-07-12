7facc560c09bf8895b3705ced2542515a7d9b3e6
Serhii Pavlovych <programmersp@gmail.com>
2020-07-12 23:28:26 +0300

    Change vue config: add public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    lintOnSave: false,
     transpileDependencies: [
         'vuetify',
     ],
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue-malyk-web/' : '/',
+
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-malyk-web/' : '/'
 };
