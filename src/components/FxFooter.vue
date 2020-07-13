40e5dae172cd7b1e66fa37667446c345a4507b30
Pavlovych Serhii <programmersp@gmail.com>
2020-07-13 22:45:13 +0300

    Add telegram button for footer component

--- src/components/FxFooter.vue
+++ src/components/FxFooter.vue
@@ -1,10 +1,18 @@
 <template>
-  <v-footer padless fixed color="#FFF8E1">
-    <v-card class="flex" tile >
+  <v-footer padless elevation='6' fixed color="#FFF8E1">
+    <v-card class="flex" tile>
       <v-card-title class="pa-0">
-          <span class="ml-1 text-sm-subtitle-2 font-weight-light">Приєднуйтесь до нас</span>
+        <!-- <span class="ml-1 text-sm-subtitle-2 font-weight-light">Приєднуйтесь до нас</span> -->
         <v-spacer></v-spacer>
-        <v-btn v-for="icon in icons" :href="icon.path" :key="icon.key" class="mx-4" color="primary"  dark icon>
+        <v-btn
+          v-for="icon in icons"
+          :href="icon.path"
+          :key="icon.key"
+          class="mx-4"
+          color="primary"
+          dark
+          icon
+        >
           <v-icon size="24px">{{ icon.icon }}</v-icon>
         </v-btn>
       </v-card-title>
@@ -17,8 +25,21 @@ export default {
   name: "fx-footer",
   data: () => ({
     icons: [
-      {key: 0, icon:"mdi-facebook", path:'https://www.facebook.com/MalykPartners/'},
-      {key: 1, icon:"mdi-instagram", path:'https://instagram.com/malykpartners'}
+      {
+        key: 0,
+        icon: "mdi-facebook",
+        path: "https://www.facebook.com/MalykPartners/"
+      },
+      {
+        key: 1,
+        icon: "mdi-instagram",
+        path: "https://instagram.com/malykpartners"
+      },
+      {
+        key: 2,
+        icon: "mdi-telegram",
+        path: "tg://resolve?domain=MalykOleksandr"
+      }
     ]
   })
 };
