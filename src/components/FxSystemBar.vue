2de153be4e79dadceef89c55f8728c05f2b090c4
Serhii Pavlovych <programmersp@gmail.com>
2020-07-14 00:28:51 +0300

    Add css style to text

--- src/components/FxSystemBar.vue
+++ src/components/FxSystemBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <v-system-bar color="amber darken-3" :height="height" :lightsOut="lightsOut" :window="window">
-      <v-icon small color="primary" class="ml-2">mdi-map-marker</v-icon><span class="white--text mr-6 text-xs-caption text-sm-h6 text-decoration-none">м. Вінниця, вул. Грушевського, 17</span>
+      <v-icon small color="primary" class="ml-2">mdi-map-marker</v-icon><span class="white--text mr-6 text text-xs-caption text-sm-h6 text-decoration-none">м. Вінниця, вул. Грушевського, 17</span>
       <v-spacer></v-spacer>
-      <v-icon small color="primary">mdi-phone</v-icon><span class="mr-6 text-xs-overline text-sm-h6"><a class="white--text text-decoration-none" href="tel:+380967606922">+38 (096) 7606922</a></span>
+      <v-icon small color="primary">mdi-phone</v-icon><span class="mr-6 text text-xs-overline text-sm-h6"><a class="white--text text-decoration-none" href="tel:+380967606922">+38 (096) 7606922</a></span>
     </v-system-bar>
   </div>
 </template>
@@ -20,4 +20,7 @@ export default {
 </script>
 
 <style scoped>
+.text{
+  font-size: 0.7rem;
+}
 </style>
