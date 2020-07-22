3c9369bbb09c9a2b7648c3aabf2a5ab98194282f
Serhii Pavlovych <programmersp@gmail.com>
2020-07-23 00:44:06 +0300

    Change carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,5 @@
 <template>
-  <v-card
-    elevation="24"
-    max-width="444"
-    class="mx-auto"
-  >
+  <v-card elevation="0" max-width="444" class="mx-auto">
     <v-system-bar lights-out></v-system-bar>
     <v-carousel
       :continuous="false"
@@ -13,66 +9,30 @@
       delimiter-icon="mdi-minus"
       height="300"
     >
-      <v-carousel-item
-        v-for="(slide, i) in slides"
-        :key="i"
-      >
-        <v-sheet
-          :color="colors[i]"
-          height="100%"
-          tile
-        >
-          <v-row
-            class="fill-height"
-            align="center"
-            justify="center"
-          >
-            <div class="display-3">{{ slide }} Slide</div>
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet color="white red--text" height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="body-2">{{ slide }}</div>
           </v-row>
         </v-sheet>
       </v-carousel-item>
     </v-carousel>
-    <v-list two-line>
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>John Leider</v-list-item-title>
-          <v-list-item-subtitle>Author</v-list-item-subtitle>
-        </v-list-item-content>
-        <v-list-item-action>
-          <v-switch
-            v-model="cycle"
-            label="Cycle Slides"
-            inset
-          ></v-switch>
-        </v-list-item-action>
-      </v-list-item>
-    </v-list>
   </v-card>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        colors: [
-          'green',
-          'secondary',
-          'yellow darken-4',
-          'red lighten-2',
-          'orange darken-1',
-        ],
-        cycle: false,
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-      }
-    },
+export default {
+  data() {
+    return {
+      cycle: true,
+      slides: [
+        "Досвідчена команда",
+        "Робота на результат",
+        "Інноваційність",
+        "Прозорість",
+        "Зручність"
+      ]
+    };
   }
+};
 </script>
