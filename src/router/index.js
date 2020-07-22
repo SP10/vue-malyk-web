84d3188d5ff2986038f4e2f5ff092177c870dd6e
Serhii Pavlovych <programmersp@gmail.com>
2020-07-23 00:44:31 +0300

    Add path routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-
+import About from "@/components/About";
+import Home from "@/components/Home";
+import Team from "@/components/Team";
 Vue.use(VueRouter);
 
 const routes = [
-
+    { path: '/home', component: Home },
+    { path: '/about', component: About },
+    { path: '/team', component: Team }
 ];
 
 const router = new VueRouter({
-  routes,
+    routes,
 });
 
 export default router;
