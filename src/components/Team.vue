e46aa77b696c3755688e5d7044a34ae1fa2a4451
Pavlovych Serhii <programmersp@gmail.com>
2020-07-23 23:31:35 +0300

    Add partners describe

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,20 +1,51 @@
 <template>
   <div>
     <v-container>
-      <v-row dense>
-        <v-col v-for="(item, i) in items" :key="i" cols="12">
-          <v-card :color="item.color" dark>
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title class="text-subtitle-2 headline" v-text="item.title"></v-card-title>
+      <v-row>
+        <v-col>
+          <v-card class="mx-auto" max-width="400">
+            <v-img class="white--text align-end" src="../assets/partner-1.png"></v-img>
 
-                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-              </div>
+            <v-card-text class="text--primary">
+              <div class="display-1 text--primary">Малик Олександр Володимирович</div>
+              <div class="pt-5">Керуючий партнер, адвокат</div>
+            </v-card-text>
 
-              <v-list-item-avatar class="ma-3" size="125" tile>
-                <v-img :src="item.src"></v-img>
-              </v-list-item-avatar>
-            </div>
+            <v-card-actions>
+              <v-btn color="primary" text>Детальніше</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card class="mx-auto" max-width="400">
+            <v-img class="white--text align-end" src="../assets/partner-2.png"></v-img>
+
+            <v-card-text class="text--primary">
+              <div class="display-1 text--primary">Гаврилюк Тетяна Дмитрівна</div>
+              <div class="pt-5">Партнер, юрист</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="primary" text>Детальніше</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card class="mx-auto" max-width="400">
+            <v-img class="white--text align-end" src="../assets/partner-3.png"></v-img>
+
+            <v-card-text class="text--primary">
+              <div class="display-1 text--primary">Сороколіта Олександр Павлович</div>
+              <div class="pt-5">Партнер, юрист</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="primary" text>Детальніше</v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
@@ -28,7 +59,7 @@ export default {
     items: [
       {
         color: "#1F7087",
-        src: "../assets/team-1.png",
+        src: "../assets/FOT_6826.JPG",
         title: "Малик Олександр Володимирович",
         artist: "Керуючий партнер, адвокат"
       },
